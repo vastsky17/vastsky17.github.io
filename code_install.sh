f2e6e2904ec3fcc40310d04d2fb24f1dec67d710
EricZhou <dejavuzhou@qq.com>
2019-07-15 11:11:10 +0800

    bash
    Signed-off-by: EricZhou <dejavuzhou@qq.com>

---
+++ code_install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+#echo "开始发布新的主题";
+
+SOURCE='/mnt/c/Users/zhouqing1/Documents/GitHub/libragen.cn';
+DEST='/mnt/c/Users/zhouqing1/go/src/github.com/dejavuzhou/dejavuzhou.github.io'
+
+rm -rf $DEST/_includes;
+rm -rf $DEST/_layouts;
+rm -rf $DEST/api;
+rm -rf $DEST/assets/css;
+rm -rf $DEST/assets/js;
+rm -rf $DEST/assets/fonts;
+
+cp -r $SOURCE/_includes $DEST/_includes;
+cp -r $SOURCE/_layouts $DEST/_layouts;
+cp -r $SOURCE/api $DEST/api;
+cp -r $SOURCE/assets/css $DEST/assets/css;
+cp -r $SOURCE/assets/js $DEST/assets/js;
+cp -r $SOURCE/assets/fonts $DEST/assets/fonts;
