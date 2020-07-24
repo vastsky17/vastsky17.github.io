930985666cb9ebc4792fc29efecc475268b20ebd
EricZhou <dejavuzhou@qq.com>
2020-07-24 23:40:48 +0800

    chinese time zone

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
-import "gojekyll/cmd"
+import (
+	"gojekyll/cmd"
+	"log"
+	"time"
+)
 
 var buildTime, gitHash string
 
+func init() {
+	l, err := time.LoadLocation("Asia/Shanghai")
+	if err != nil {
+		log.Println(err)
+	} else {
+		log.Println("chinese time zone")
+		time.Local = l
+	}
+}
 func main() {
 	cmd.Execute(buildTime, gitHash)
 }
